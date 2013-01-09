f5529a2cff6f2762832d0176fb1558a195742313
marc.weistroff <marc.weistroff@sensio.com>
2013-01-08 23:11:18 -0500

    FMT check_test.go

--- check_test.go
+++ check_test.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-    "testing"
-    "net/http"
-    "net/http/httptest"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
 type successPollHandler struct {
@@ -14,14 +14,14 @@ func (p successPollHandler) ServeHTTP(http.ResponseWriter, *http.Request) {
 
 func TestPollIsContactingHttpServer(t *testing.T) {
 	server := httptest.NewServer(successPollHandler{})
-    defer server.Close()
+	defer server.Close()
 
 	check, _ := NewCheck(server.URL, "foobar", "10s", make(map[string]string))
 	statusCode, _, err := check.Poll(&http.Client{})
-    if err != nil {
-        t.Error(err)
+	if err != nil {
+		t.Error(err)
 	}
-    if statusCode != 200 {
-        t.Error("statusCode should be 200")
+	if statusCode != 200 {
+		t.Error("statusCode should be 200")
 	}
 }
