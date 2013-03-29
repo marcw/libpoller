e553d44a57294eb5b7da5685337096ee563799f8
Marc Weistroff <marc@weistroff.net>
2013-03-29 16:42:28 -0400

    Update documentation

--- poller.go
+++ poller.go
@@ -1,18 +1,25 @@
 package poller
 
+// An Alerter raises an alert based on the event it received.
+// An alert is a communication to a system or a user with the information about current's and past check's states.
+// For concrete implementation, see the "github.com/marcw/poller/alert" package.
 type Alerter interface {
 	Alert(event *Event)
 }
 
+// A backend log checks event.
+// For concrete implementation, see the "github.com/marcw/poller/backend" package.
 type Backend interface {
 	Log(e *Event)
 	Close()
 }
 
+// A Poller is the glue between a Scheduler, a Backend, a Service and a Alerter. 
 type Poller interface {
 	Run(Scheduler, Backend, Service, Alerter)
 }
 
+// A Service is a specialized way to poll a check. ie: a HttpService will specialize in polling HTTP resources.
 type Service interface {
 	Poll(c *Check) *Event
 }
@@ -20,6 +27,8 @@ type Service interface {
 type directPoller struct {
 }
 
+// NewDirectPoller() returns a "no-frills" Poller instance. 
+// It waits for the next scheduled check, poll it, log it and if alerting is needed, pass it through the alerter.
 func NewDirectPoller() Poller {
 	return &directPoller{}
 }
